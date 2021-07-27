067f391e72069bbdc1c8fc9c2ef6a7e993cb2fb8
omerrfaruk1 <62781031+omerrfaruk1@users.noreply.github.com>
2021-07-27 23:24:41 +0300

    Update App.js

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
 class Request{
 	async get(url){
-	const response = await fetch(url); // Response Yapısı
+		const response = await fetch(url); // Response Yapısı
         const data = await response.json(); // Json Objesi
-        return data;
+		return data;
 	}
+	
 }
+
 const request = new Request;
-const name = document.getElementsByClassName("name")
-const fiyat = document.getElementsByClassName("price")   
+const name = ["bitcoin","ethereum","ripple","stellar","polkadot","tether","chainlink","litecoin","binancecoin","cardano"]
+const fiyat = document.getElementsByClassName("price")
 const hacim = document.getElementsByClassName("hacim")   
 const degisim = document.getElementsByClassName("değişim") 
 function getData(){
 	request.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cripple%2Ccardano%2Cstellar%2Cpolkadot%2Cbinancecoin%2Clitecoin%2Cchainlink&vs_currencies=usd&include_market_cap=true&include_24hr_change=true')
 	.then(x1 => {
-		for (i=0; i<fiyat.length; i++){
+
+		experimentObjcetToArray();
+		for (i=0; i<name.length; i++){
 			updatePrice(i,x1);
-		}; 
-	})
+			
+			}; 
+		})
 	.catch(err => console.log(err));
-}
-setInterval(getData,2000);
+	}
+	getData()
+
 
 async function updatePrice(i,x1){
-		let b1 = (x1[name[i].textContent].usd);
-		let b2 = (x1[name[i].textContent].usd_market_cap);
-		let b3 = (x1[name[i].textContent].usd_24h_change);
-			fiyat[i].textContent = b1 + " "+ "$";
-			hacim[i].textContent = Math.ceil(b2)+" "+ "$";
-			degisim[i].textContent = Number(b3.toFixed(2));
-				if (degisim[i].textContent < 0){
+		let b1 = (x1[name[i]].usd);
+		let b2 = (x1[name[i]].usd_market_cap);
+		let b3 = (x1[name[i]].usd_24h_change);
+		const tbody = document.getElementById("tbody");
+		tbody.innerHTML += `
+			<tr>
+            <th class="name">${i+1}</th>
+            <td class="price">${name[i]}</td>
+            <td class="price">${b1 + " "+ "$"}</td>
+            <td class="hacim">${Math.ceil(b2)+" "+ "$"}</td>
+            <td class="değişim">${Number(b3.toFixed(2))}</td> 
+          </tr>
+		`		
+				if (b3 < 0){
 					degisim[i].style = "color:red"
 				}  
 				else{
@@ -36,4 +49,8 @@ async function updatePrice(i,x1){
 				}
 
 }
+async function experimentObjcetToArray(x1){
+	
+}
+
 
